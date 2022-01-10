d88892739b7579c102a98abe91072b29b2e17ebe
digas111 <37022052+digas111@users.noreply.github.com>
2022-01-10 11:32:40 +0000

    Initial Commit

---
+++ Shared/slot_machineApp.swift
@@ -0,0 +1,17 @@
+//
+//  slot_machineApp.swift
+//  Shared
+//
+//  Created by Diogo Ribeiro on 10/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct slot_machineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
